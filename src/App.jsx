1fa40a9d91f604fe23b8a34d4b19099a198828fa
Luka Jincharadze <lukajinchara@gmail.com>
2023-12-03 20:39:02 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => alert("check in")}>Check in</Button>
+        <Button onClick={() => alert("check out")}>Check out</Button>
+
+        <Heading as="h3">Forms</Heading>
+        <Input type="number" placeholder="Number of guests " />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
