e062adf9ae9bdd09b5df43b53474840e8d775236
Luka Jincharadze <lukajinchara@gmail.com>
2023-12-06 22:23:28 +0400

    update
    backend with supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://kddvxvzkmhqoqurjbrig.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkZHZ4dnprbWhxb3F1cmpicmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE4ODI3NjcsImV4cCI6MjAxNzQ1ODc2N30.c_5A7nOW1_ouUhafiYTPMNE7UxjXYQnXOMuqEzMHDlw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
