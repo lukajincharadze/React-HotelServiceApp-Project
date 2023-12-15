1a6ba275ced8889ff12309ef3b36506f9163b11a
Luka Jincharadze <lukajinchara@gmail.com>
2023-12-15 22:53:36 +0400

    Update apiCabins.js

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -20,8 +20,7 @@ export async function createEditCabin(newCabin, id) {
   );
   const imagePath = hasImagePath
     ? newCabin.image
-    : `${supabaseUrl}/storage/v1/object/public/cabin-images/
-  ${imageName}`;
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
   // 1. Create/edit cabin
   let query = supabase.from("cabins");
