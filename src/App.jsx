820c33a1d15e0d317ab4a6e8ae65976ff32a43d3
Luka Jincharadze <lukajinchara@gmail.com>
2023-12-28 18:28:03 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "./styles/GlobalStyles";
@@ -16,7 +22,6 @@ import { Toaster } from "react-hot-toast";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 60 * 1000,
       staleTime: 0,
     },
   },
@@ -28,10 +33,16 @@ function App() {
       <ReactQueryDevtools initialIsOpen={false} />
 
       <GlobalStyles />
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="dashboard" />} />
+          <Route
+            path="/"
+            element={
+              <AppLayout>
+                <Navigate to="dashboard" replace />
+              </AppLayout>
+            }
+          >
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
@@ -43,7 +54,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
       <Toaster
         position="top-center"
         gutter={12}
